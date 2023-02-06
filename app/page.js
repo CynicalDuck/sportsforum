01694b4e1c6215118eed9d9f5dac37540b6abe3c
Marius Bekk <marius.bekk@intility.no>
2023-02-06 12:52:45 +0100

    Adding Supabase

--- app/page.js
+++ app/page.js
@@ -4,13 +4,8 @@ import { Blog, Hero } from "../sections";
 const Page = () => (
   <div className="bg-gradient-to-tl from-gray-600 via-gray-700 to-gray-900 overflow-hidden">
     <Navbar />
-    <div className="grid gap-4 lg:grid-cols-7 sm:grid-cols-1">
-      <div className="col-span-6">
-        <Blog />
-      </div>
-      <Sidebar />
-      <div className="gradient-03 z-0" />
-    </div>
+    <Blog />
+    <div className="gradient-03 z-0" />
     <div className="relative">
       <div className="gradient-04 z-0" />
     </div>
